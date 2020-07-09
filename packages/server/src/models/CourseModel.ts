a766631f7b2a3568077b9758bfde5bc65d8f4e8d
AugustoCalaca <augustocalaca2@gmail.com>
2020-07-09 00:00:49 -0300

    feat(server): add coure model

---
+++ packages/server/src/models/CourseModel.ts
@@ -0,0 +1,55 @@
+import mongoose, { Document, Model } from 'mongoose';
+
+const CourseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      index: true,
+      default: 'Awesome title',
+    },
+    subtitle: {
+      type: String,
+      required: true,
+      index: true,
+      default: 'Awesome subtitle',
+    },
+    description: {
+      type: String,
+      required: true,
+      default: 'Awesome description',
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    startedAt: {
+      type: Date,
+      required: true,
+      default: new Date(),
+    },
+  },
+  {
+    timestamps: {
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt',
+    },
+    collection: 'Course',
+  },
+);
+
+export interface ICourse extends Document {
+  title: string;
+  subtitle: string;
+  description: string;
+  isActive: boolean;
+  startedAt: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+CourseSchema.index({ title: 'text', subtitle: 'text' });
+
+const CourseModel: Model<ICourse> = mongoose.model('Course', CourseSchema);
+
+export default CourseModel;
