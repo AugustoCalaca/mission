892ed379d2200be2745c61fe4879ebd0950f92e7
AugustoCalaca <augustocalaca2@gmail.com>
2020-07-09 00:07:23 -0300

    feat(server): add helpers to messages and page info

---
+++ packages/server/src/api/helpers.ts
@@ -0,0 +1,82 @@
+import { Context } from 'koa';
+import { Model } from 'mongoose';
+
+export const MESSAGE = {
+  INVALID_ID: 'Invalid id',
+  ERROR: 'ERROR',
+  OK: 'OK',
+  PAGE_INFO: {
+    ERRORS: {
+      NEGATIVE: 'Pagination values should be positive values',
+    },
+  },
+  FIND: {
+    ERRORS: 'Missing param `q` to find results',
+  },
+  COURSE: {
+    CREATING: 'Some error occurred while creating course',
+    MISSING: 'Missing course',
+    NOT_FOUND: 'Course not found',
+    UPDATING: 'Some error occurred while updating course',
+    DELETING: 'Some error occurred while deleting course',
+  },
+};
+
+export const getPageAndPageSize = (ctx: Context) => {
+  const { page = 1, pageSize = 10 } = ctx.query;
+
+  if (page < 1 || pageSize < 1) {
+    return {
+      skips: null,
+      page: null,
+      pageSize: null,
+      errors: [
+        {
+          data: { page, pageSize },
+          message: MESSAGE.PAGE_INFO.ERRORS.NEGATIVE,
+        },
+      ],
+    };
+  }
+
+  const pageInt = parseInt(page, 10);
+  const mongoSkips = (pageInt - 1) * pageSize;
+  const mongoLimit = parseInt(pageSize, 10);
+
+  return {
+    skips: mongoSkips,
+    page: pageInt,
+    pageSize: mongoLimit,
+    errors: null,
+  };
+};
+
+export const getPageInfo = async (ctx: Context, model: Model<any>) => {
+  const { errors, skips, page, pageSize } = getPageAndPageSize(ctx);
+
+  if (errors) {
+    return {
+      errors,
+      skips,
+      page,
+      pageSize,
+      totalCount: null,
+      hasNextPage: null,
+      hasPreviousPage: null,
+    };
+  }
+
+  const totalCount = await model.count({});
+  const hasPreviousPage = skips > 0;
+  const hasNextPage = skips + pageSize < totalCount;
+
+  return {
+    errors: null,
+    skips,
+    page,
+    pageSize,
+    totalCount,
+    hasNextPage,
+    hasPreviousPage,
+  };
+};
