47a7e3f36fb108d08e3904110d64be830ed5dd30
AugustoCalaca <augustocalaca2@gmail.com>
2020-07-09 00:12:48 -0300

    feeat(server): exports all methods

---
+++ packages/server/src/api/course/index.ts
@@ -0,0 +1,7 @@
+export { courseGet } from './get';
+export { coursesGetAll } from './getAll';
+export { coursesGetFind } from './getFind';
+export { coursePost } from './post';
+export { coursePut } from './put';
+export { coursePatch } from './patch';
+export { courseDelete } from './delete';
