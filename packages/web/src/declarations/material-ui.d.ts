4eb6a81188baebd47d04a1cd13d16030969825fd
AugustoCalaca <augustocalaca2@gmail.com>
2020-07-12 17:14:52 -0300

    feat(server): add type declarations

---
+++ packages/web/src/declarations/material-ui.d.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/interface-name-prefix */
+import '@material-ui/core/styles/createMuiTheme';
+
+declare module '@material-ui/core/styles/createMuiTheme' {
+  interface ThemeVariables {
+    drawerWidthFull: number;
+    drawerWidthMini: number;
+    headerHeight: number;
+    headerHeightUpSm: number;
+    tabbarHeight: number;
+    contentPadding: number;
+    contentPaddingUpSm: number;
+    boxShadow: string;
+  }
+
+  interface Theme {
+    variables?: ThemeVariables;
+  }
+
+  interface ThemeOptions {
+    variables?: ThemeVariables;
+  }
+}
