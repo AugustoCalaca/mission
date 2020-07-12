79f374913d9eaeb7789b18e95cb9334a39ecae5b
AugustoCalaca <augustocalaca2@gmail.com>
2020-07-12 17:26:00 -0300

    feat(web): add card loader

---
+++ packages/web/src/ui/CardLoader.tsx
@@ -0,0 +1,16 @@
+import React, { memo } from 'react';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+const useStyle = makeStyles({
+  container: {
+    height: 5,
+  },
+});
+
+const CardLoader = (props: { show: boolean }) => {
+  const classes = useStyle(props);
+  return <div className={classes.container}>{props.show && <LinearProgress color="primary" />}</div>;
+};
+
+export default memo(CardLoader);
